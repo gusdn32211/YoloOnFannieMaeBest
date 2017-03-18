9ebf42389ca85e81368f38f93097dda944ff9cc8
Clarence Lam <clarencelam95@gmail.com>
2017-03-18 15:46:19 -0700

    adding reducer

--- src/store.js
+++ src/store.js
@@ -3,9 +3,15 @@ import reducer from "./reducers";
 import logger from "redux-logger"
 import thunk from "redux-thunk";
 import promise from "redux-promise-middleware"
+import axios from "axios";
 
 const middleware = applyMiddleware(promise(), thunk, logger())
 
 const store = createStore(reducer, middleware);
+
+store.dispatch({
+	type: "FETCH_DATA",
+	payload: axios.get("http://rest.learncode.academy/api/wstern/users")
+})
 
 export default store;
