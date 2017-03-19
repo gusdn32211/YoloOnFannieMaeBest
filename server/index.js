d209475e9278e29ddd787fae48f472bb53e7c468
Alexander Lim <alexsclim@gmail.com>
2017-03-19 00:55:16 -0700

    fix

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 let app = express();
@@ -18,7 +17,6 @@ app.use(webpackMiddleware(compiler, {
   publicPath: webpackConfig.output.publicPath,
   noInfo: true
 }));
-app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
